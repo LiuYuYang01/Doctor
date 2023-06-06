4e8ec9aae63dc8555dafb7eefd4ddbb163347924
刘宇阳 <3311118881@qq.com>
2023-06-06 10:37:13 +0800

    引入vant组件库并配置自动按需导入功能

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+// 引入vant组件库样式
+import 'vant/lib/index.css'
+// 自定义的样式
+import '@/styles/main.scss'
 
 const app = createApp(App)
 
