ea3459f225c83cfad63f05d5992dd2d6f260c84d
刘宇阳 <3311118881@qq.com>
2023-06-13 08:22:30 +0800

    渲染快捷工具布局

--- src/views/User/index.vue
+++ src/views/User/index.vue
@@ -10,6 +10,16 @@ onMounted(async () => {
     const { data } = await getUserInfo()
     userInfo.value = data
 })
+
+const tools = [
+    { label: '我的问诊', path: '/user/consult' },
+    { label: '我的处方', path: '/' },
+    { label: '家庭档案', path: '/user/patient' },
+    { label: '地址管理', path: '/user/address' },
+    { label: '我的评价', path: '/' },
+    { label: '官方客服', path: '/' },
+    { label: '设置', path: '/' }
+]
 </script>
 
 <template>
@@ -46,6 +56,7 @@ onMounted(async () => {
                 </van-col>
             </van-row>
         </div>
+
         <div class="user-page-order">
             <div class="head">
                 <h3>药品订单</h3>
@@ -80,6 +91,13 @@ onMounted(async () => {
                     <p>已完成</p>
                 </van-col>
             </van-row>
+        </div>
+
+        <div class="user-page-group">
+            <h3>快捷工具</h3>
+            <van-cell :title="label" is-link :border="false" :to="path" v-for="{ label, path } in tools">
+                <template #icon><cp-icon name="user-tool-01" /></template>
+            </van-cell>
         </div>
     </div>
 </template>
